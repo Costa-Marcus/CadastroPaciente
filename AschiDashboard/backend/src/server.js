729ac272f0a42dc059805b9450bf1ab6cabfeb28
Marcus Vinicius Cardoso Costa <marcusvinnie6@gmail.com>
2026-08-31 10:47:15 -0300

    Delete AschiDashboard/backend/src directory

--- AschiDashboard/backend/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-
-const port = Number(process.env.PORT || 3333);
-
-app.listen(port, () => {
-    console.log(`ASCHI Finance API rodando em http://localhost:${port}`);
-});
